9bc110aa11db4cf88771df470269af79c9750d7d
elvine28815 <elvine.28815@students.kyu.ac.ke>
2025-11-03 23:04:27 +0300

    Create array for loop.c

---
+++ array for loop.c
@@ -0,0 +1,9 @@
+// Array declaration and initialization: data type, name, [rows], [columns]
+int scores[4][2] = {
+    // First 2x2 block
+    {65, 92}, 
+    {84, 72},
+    // Second 2x2 block
+    {35, 70},
+    {59, 67}
+};
